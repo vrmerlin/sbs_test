5f3ca5961269182d487fb559bc690497f878b73c
vrmerlin <fisher@jfisher.com>
2014-05-31 10:22:47 -0700

    Initial Commit

---
+++ Includes/simplecms-config.php
@@ -0,0 +1,9 @@
+<?php
+    define('DB_NAME', 'sbsdb');
+    define('DB_USER', 'b0354d62dec625');
+    define('DB_PASSWORD', 'eca99b1e');
+    define('DB_HOST', 'us-cdbr-azure-west-a.cloudapp.net');
+
+    define('DEFAULT_ADMIN_USERNAME', 'admin');
+    define('DEFAULT_ADMIN_PASSWORD', '008hsif');
+?>
